ccb60646ae0434138ece7a505161eb1b3f131d2f
oluwagbenga adedire <oluwagbengaadedire@gmail.com>
2024-11-29 08:48:58 +0100

    update

--- app.js
+++ app.js
@@ -11,18 +11,6 @@ const app = express();
 
 dotenv.config();
 
-async function connectToDatabase() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB successfully");
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
-    process.exit(1); 
-  }
-}
 
 connectToDatabase();
 
